4b8c3ad9daabd02e6bfdf58802ac56b51bae48bb
zayaanzehgeer <zayaanzehgeer.gmail.com>
2024-01-17 15:44:48 +0530

    Credit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,9 +28,7 @@ const Hero = () => {
           Payment Method.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs. We examine annual percentage rates,
-          annual fees.
+          Credit for this making this website goes to the Youtube channel "JavaScript Mastery", I Zayaan Zehgeer merely cloned it as taught in the video.
         </p>
       </div>
 
